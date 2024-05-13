260289be701e04daf2345516c765d16c0d0a6a45
Vadim Malashenko <zex404@gmail.com>
2024-05-13 13:23:01 +1000

    Add form fields with labels as TranslatableMessage.

--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -3,22 +3,38 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatableMessage;
 
 class PersonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('firstName', TextType::class, [
+                'label' => new TranslatableMessage('person.firstName', [], 'person'),
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => new TranslatableMessage('person.lastName', [], 'person')
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+                'label' => new TranslatableMessage('person.description', [], 'person')
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => new TranslatableMessage('person.submit', [], 'person')
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+
         ]);
     }
 }
